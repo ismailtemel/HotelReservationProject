883955c107d1818be8a641267074072d0b7ac7c9
İsmail Temel <ismailtemel2000@gmail.com>
2022-03-24 10:55:34 +0300

    HotelProject

---
+++ TraversalCoreProject/BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        // Bussines'a gelmeden önce businnesa tanımlayacağım bütün metotlar burdan geçecek
+        void TAdd(T t);
+        void TDelete(T t);
+        void TUpdate(T t);
+        List<T> TGetList();
+        T TGetById(int id);
+    }
+}
